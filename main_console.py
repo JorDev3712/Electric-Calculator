82b4f7dfef8e5db53dc510b685728e73451d6e3c
s4archivosxx <jordan_vasquez_2019@hotmail.com>
2025-09-30 09:31:41 -0500

    correcion para cuando es archivo exe

--- main_console.py
+++ main_console.py
@@ -1,11 +1,20 @@
 import os
+import sys
 
 from console.controllers.appController import AppController
 from util.languageManager import LanguageManager
 from util.utility import Utility
 
+def get_app_folder():
+    if getattr(sys, 'frozen', False):
+        # Ejecutable empaquetado
+        return os.path.dirname(sys.executable)
+    else:
+        # Script de Python normal
+        return os.path.dirname(os.path.abspath(__file__))
+
 # Gets the root folder
-folder = os.path.dirname(os.path.abspath(__file__))
+folder = get_app_folder()
 languageManger = LanguageManager(folder, 'app.xml')
 
 # Sets common language
